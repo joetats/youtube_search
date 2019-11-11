e840893e0d09fdcd7b6d0fac24d30b1bf78f1d4a
joetats <tatuskojc@gmail.com>
2019-11-11 10:36:10 -0500

    bump version, add max_fields

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="youtube-search",
-    version="0.1.1",
+    version="0.1.2",
     description="Perform YouTube video searches without the API",
     long_description=README,
     long_description_content_type="text/markdown",
--- youtube_search/__init__.py
+++ youtube_search/__init__.py
@@ -2,19 +2,39 @@ import requests
 from bs4 import BeautifulSoup
 import urllib.parse
 
+
 class YoutubeSearch:
 
-    def __init__(self, search_terms):
+    def __init__(self, search_terms: str, max_results=None):
         self.search_terms = search_terms
+        self.max_results = max_results
 
     def search(self):
         encoded_search = urllib.parse.quote(self.search_terms)
-        url = f'https://youtube.com/results?search_query={encoded_search}&pbj=1'
-        res = requests.get(url)
-        soup = BeautifulSoup(res.text)
+        BASE_URL = 'https://youtube.com'
+        url = f'{BASE_URL}/results?search_query={encoded_search}&pbj=1'
+        results = self.parse_html(BeautifulSoup(requests.get(url).text))
+        if self.max_results is not None and len(results) > self.max_results:
+            self.videos = results[:self.max_results]
+        else:
+            self.videos = results
 
-        self.videos = [(video['title'], video['href']) for video in soup.select('.yt-uix-tile-link')]
+    def parse_html(self, soup):
+        results = []
+        for video in soup.select('.yt-uix-tile-link'):
+            if '=' in video['href']:
+                video_info = {
+                    'title': video['title'],
+                    'link': video['href'],
+                    'id': video['href'][video['href'].index('=')+1:]
+                }
+                results.append(video_info)
+        return results
 
-    def json(self):
+    def to_dict(self):
+        self.search()
+        return self.videos
+
+    def to_json(self):
         self.search()
         return self.videos
