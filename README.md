b293c4d8783bffd7fad39a8f01400a23a34770b6
joetats <tatuskojc@gmail.com>
2019-11-12 10:34:30 -0500

    update readme, make minor fixes

--- README.md
+++ README.md
@@ -2,4 +2,18 @@
 
 Python function for searching for youtube videos to avoid using their heavily rate-limited API
 
-This basically scrapes the search results from the website, and will have more and more customization as I get around to adding it.
+To avoid using the API, this uses the form on the youtube homepage and scrapes the resulting page.
+
+## Example Usage
+
+For a basic search (and all of the current functionality), you can use the search tool as follows:
+
+```pip install youtube-search```
+
+```python
+from youtube_search import YoutubeSearch
+
+results = YoutubeSearch('search terms', max_results=10).to_json()
+
+print(results)
+```
--- youtube_search/__init__.py
+++ youtube_search/__init__.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import urllib.parse
+import json
 
 
 class YoutubeSearch:
@@ -8,33 +9,32 @@ class YoutubeSearch:
     def __init__(self, search_terms: str, max_results=None):
         self.search_terms = search_terms
         self.max_results = max_results
+        self.videos = self.search()
 
     def search(self):
         encoded_search = urllib.parse.quote(self.search_terms)
-        BASE_URL = 'https://youtube.com'
-        url = f'{BASE_URL}/results?search_query={encoded_search}&pbj=1'
-        results = self.parse_html(BeautifulSoup(requests.get(url).text))
+        BASE_URL = "https://youtube.com"
+        url = f"{BASE_URL}/results?search_query={encoded_search}&pbj=1"
+        response = BeautifulSoup(requests.get(url).text, "html.parser")
+        results = self.parse_html(response)
         if self.max_results is not None and len(results) > self.max_results:
-            self.videos = results[:self.max_results]
-        else:
-            self.videos = results
+            return results[:self.max_results]
+        return results
 
     def parse_html(self, soup):
         results = []
-        for video in soup.select('.yt-uix-tile-link'):
-            if '=' in video['href']:
+        for video in soup.select(".yt-uix-tile-link"):
+            if video["href"].startswith("/watch?v="):
                 video_info = {
-                    'title': video['title'],
-                    'link': video['href'],
-                    'id': video['href'][video['href'].index('=')+1:]
+                    "title": video["title"],
+                    "link": video["href"],
+                    "id": video["href"][video["href"].index("=")+1:]
                 }
                 results.append(video_info)
         return results
 
     def to_dict(self):
-        self.search()
         return self.videos
 
     def to_json(self):
-        self.search()
-        return self.videos
+        return json.dumps({"videos": self.videos})
