97713df6f58ccd24490c5434d6abdc246cfd030c
Tatusko <jtatusko@idirect.net>
2020-07-09 12:01:40 -0400

    fix youtube data issue, update version

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 # Version
-__version__ = "0.1.4"
+__version__ = "1.0.0"
--- youtube_search/__init__.py
+++ youtube_search/__init__.py
@@ -1,5 +1,4 @@
 import requests
-from bs4 import BeautifulSoup
 import urllib.parse
 import json
 
@@ -14,28 +13,37 @@ class YoutubeSearch:
     def search(self):
         encoded_search = urllib.parse.quote(self.search_terms)
         BASE_URL = "https://youtube.com"
-        url = f"{BASE_URL}/results?search_query={encoded_search}&pbj=1"
-        response = BeautifulSoup(requests.get(url).text, "html.parser")
+        url = f"{BASE_URL}/results?search_query={encoded_search}"
+        response = requests.get(url).text
+        while 'window["ytInitialData"]' not in response:
+            response = requests.get(url).text
         results = self.parse_html(response)
         if self.max_results is not None and len(results) > self.max_results:
             return results[:self.max_results]
         return results
 
-    def parse_html(self, soup):
+    def parse_html(self, response):
         results = []
-        for video_div in soup.select("div.yt-lockup-content"):
-            video = video_div.select_one(".yt-uix-tile-link")
-            if video is not None:
-                if video["href"].startswith("/watch?v="):
-                    channel = video_div.select_one("a.spf-link")
-                    video_info = {
-                        "title": video["title"],
-                        "link": video["href"],
-                        "id": video["href"][video["href"].index("=")+1:],
-                        "channel_name": channel.text,
-                        "channel_link": channel["href"]
-                    }
-                    results.append(video_info)
+        start = response.index('window["ytInitialData"]') + len('window["ytInitialData"]') + 3
+        end = response.index('};', start) + 1
+        json_str = response[start:end]
+        data = json.loads(json_str)
+
+        videos = data['contents']['twoColumnSearchResultsRenderer']['primaryContents']['sectionListRenderer']['contents'][0]['itemSectionRenderer']['contents']
+
+        for video in videos:
+            res = {}
+            if 'videoRenderer' in video.keys():
+                video_data = video['videoRenderer']
+                res['id'] = video_data['videoId']
+                res['thumbnails'] = [thumb['url'] for thumb in video_data['thumbnail']['thumbnails']]
+                res['title'] = video_data['title']['runs'][0]['text']
+                res['long_desc'] = video_data['descriptionSnippet']['runs'][0]['text']
+                res['channel'] = video_data['longBylineText']['runs'][0]['text']
+                res['duration'] = video_data.get('lengthText', {}).get('simpleText', 0)
+                res['views'] = video_data.get('viewCountText', {}).get('simpleText', 0)
+                res['url_suffix'] = video_data['navigationEndpoint']['commandMetadata']['webCommandMetadata']['url']
+                results.append(res)
         return results
 
     def to_dict(self):
