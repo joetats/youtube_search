e4e849813ca2ac681e8e78f0e6d3f75e84494c7d
PLUJA <64632615+pluja@users.noreply.github.com>
2020-08-17 09:11:54 +0200

    Add support for channels and add publishedText to videos.

--- youtube_search/__init__.py
+++ youtube_search/__init__.py
@@ -7,21 +7,61 @@ class YoutubeSearch:
     def __init__(self, search_terms: str, max_results=None):
         self.search_terms = search_terms
         self.max_results = max_results
-        self.videos = self.search()
+        self.videos = self.search_videos()
+        self.channels = self.search_channels()
 
-    def search(self):
+    def search_videos(self):
         encoded_search = urllib.parse.quote(self.search_terms)
         BASE_URL = "https://youtube.com"
         url = f"{BASE_URL}/results?search_query={encoded_search}"
         response = requests.get(url).text
         while 'window["ytInitialData"]' not in response:
             response = requests.get(url).text
-        results = self.parse_html(response)
+        results = self.parse_html_videos(response)
         if self.max_results is not None and len(results) > self.max_results:
             return results[: self.max_results]
         return results
 
-    def parse_html(self, response):
+    def search_channels(self):
+        encoded_search = urllib.parse.quote(self.search_terms)
+        BASE_URL = "https://youtube.com"
+        url = f"{BASE_URL}/results?search_query={encoded_search}"
+        response = requests.get(url).text
+        while 'window["ytInitialData"]' not in response:
+            response = requests.get(url).text
+        results = self.parse_html_channels(response)
+        if self.max_results is not None and len(results) > self.max_results:
+            return results[: self.max_results]
+        return results
+
+    def parse_html_channels(self, response):
+        results = []
+        start = (
+            response.index('window["ytInitialData"]')
+            + len('window["ytInitialData"]')
+            + 3
+        )
+        end = response.index("};", start) + 1
+        json_str = response[start:end]
+        data = json.loads(json_str)
+
+        datalist = data["contents"]["twoColumnSearchResultsRenderer"]["primaryContents"][
+            "sectionListRenderer"
+        ]["contents"][0]["itemSectionRenderer"]["contents"]
+
+        for channel in datalist:
+            res = {}
+            if "channelRenderer" in channel.keys():
+                channel_data = channel.get("channelRenderer", {})
+                res["id"] = channel_data.get("channelId", None)
+                res["name"] = channel_data.get("title", None).get("simpleText", None)
+                res["suscriberCountText"] = channel_data.get("subscriberCountText", None).get("simpleText", None).split(" ")[0]
+                res["thumbnails"] = [thumb.get("url", None) for thumb in channel_data.get("thumbnail", {}).get("thumbnails", [{}]) ]
+                res["url_suffix"] = channel_data.get("navigationEndpoint", {}).get("commandMetadata", {}).get("webCommandMetadata", {}).get("url", None)
+                results.append(res)
+        return results
+
+    def parse_html_videos(self, response):
         results = []
         start = (
             response.index('window["ytInitialData"]')
@@ -46,13 +86,17 @@ class YoutubeSearch:
                 res["long_desc"] = video_data.get("descriptionSnippet", {}).get("runs", [{}])[0].get("text", None)
                 res["channel"] = video_data.get("longBylineText", {}).get("runs", [[{}]])[0].get("text", None)
                 res["duration"] = video_data.get("lengthText", {}).get("simpleText", 0)
-                res["views"] = video_data.get("viewCountText", {}).get("simpleText", 0) 
+                res["views"] = video_data.get("viewCountText", {}).get("simpleText", 0)
+                res['publishedText'] = video_data.get("publishedTimeText", None).get("simpleText")
                 res["url_suffix"] = video_data.get("navigationEndpoint", {}).get("commandMetadata", {}).get("webCommandMetadata", {}).get("url", None)
                 results.append(res)
         return results
 
-    def to_dict(self):
+    def videos_to_dict(self):
         return self.videos
+
+    def channels_to_dict(self):
+        return self.channels
 
     def to_json(self):
         return json.dumps({"videos": self.videos})
