cd0596a4830332843ca3c7c3560b360654d2dadc
DaWe <10603822+DaWe35@users.noreply.github.com>
2020-09-20 23:20:41 +0200

    add publish_time to return elements

--- youtube_search/__init__.py
+++ youtube_search/__init__.py
@@ -46,7 +46,8 @@ class YoutubeSearch:
                 res["long_desc"] = video_data.get("descriptionSnippet", {}).get("runs", [{}])[0].get("text", None)
                 res["channel"] = video_data.get("longBylineText", {}).get("runs", [[{}]])[0].get("text", None)
                 res["duration"] = video_data.get("lengthText", {}).get("simpleText", 0)
-                res["views"] = video_data.get("viewCountText", {}).get("simpleText", 0) 
+                res["views"] = video_data.get("viewCountText", {}).get("simpleText", 0)
+                res["publish_time"] = video_data.get("publishedTimeText", {}).get("simpleText", 0)
                 res["url_suffix"] = video_data.get("navigationEndpoint", {}).get("commandMetadata", {}).get("webCommandMetadata", {}).get("url", None)
                 results.append(res)
         return results
